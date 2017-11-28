354ab10b678456132f30e00ecd0adc3deeeff17b
HarlanZhou <798895848@qq.com>
2017-11-28 08:27:25 +0800

    聊天界面可以根据用户头像名字自动导入

--- 微信/wechat/js/router.js
+++ 微信/wechat/js/router.js
@@ -101,8 +101,48 @@ $(function(){
 			$(".items").click(function(){
 				
 				//获取话语
-				var word = $(this).find("items-msg-content").html()
-				$("#dynamic").load("page/dynamic/dialogue.html")
+				//var src = "url(img/chat/user/lwz.jpg);"
+				var src = $(this).data("image-src")
+				$("#dynamic").load("page/dynamic/dialogue.html",function(){
+					
+	
+					var key = "b7f3afeea99c40c7a9df742f64551105";
+					var url = "http://www.tuling123.com/openapi/api";
+			
+					$('#btn').click(function() {  //当点击时
+						
+						var info = {
+							"key": key,
+							"info": $("#input-content").val() 
+						}   //向图灵 发送 聊天数据     .val  =  value
+						
+						var self = $("<div class='record flex  x-center'><div class='record-left flex x-start y-items-center position-relative'>"
+						+"</div><div class='record-center flex x-end '><div class='record-content record-font user-background '>"+$("#input-content").val()
+						+"</div></div>"+"<div class='record-right x-start y-items-center position-relative '>"
+			                        +"<div class='record-photo ' style='background-image: url(img/chat/user/myPhoto.jpg);'></div>"+"</div><br>")
+						$("#dialouge-items").append(self)
+						
+						
+			
+						$.ajax({
+							type: "POST",
+							url: url,
+							data: info,
+							dataType: "json",
+							success: function(result) {
+								var robot = $("<div class='record flex  x-center'><div class='record-left flex x-start y-items-center position-relative'> <div class='record-photo ' style='background-image: "+src
+								+"'></div></div>"
+								+"<div class='record-center flex x-start '><div class='record-content record-font friend-background '>"+result.text 
+								+"</div></div><div class='record-right flex '></div></div></br>")
+								
+								//src "url(img/chat/user/AndrewNg.jpg);"
+								$("#dialouge-items").append(robot)
+							
+							}
+						});
+			
+					})
+			    })
 				
 				
 				
